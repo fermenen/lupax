fd4a299f1bf55cbcd7d1d42814a490cac1c2e8e8
Fernando M <fermenen2010@gmail.com>
2022-12-13 18:57:58 +0100

    test

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -28,7 +28,7 @@ type AppPropsWithLayout = AppProps & {
 }
 
 if (typeof window !== 'undefined') {
-  console.info("holaaaaaaa", process.env.NEXT_PUBLIC_URL_BASE_API)
+  console.info("holaaaaaaa", process.env.NODE_ENV)
   // we only want to call this init function on the frontend, so we check typeof window !== 'undefined'
   SuperTokensReact.init({
     appInfo: {
