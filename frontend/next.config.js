b9f49764932932cd3a44930d62cbc5b1a1f25892
Fernando M <fermenen2010@gmail.com>
2022-12-13 18:20:48 +0100

    docker env

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   experimental: {
     outputStandalone: true,
   },
-  env: {
-    NEXT_PUBLIC_URL_BASE_API: process.env.NEXT_PUBLIC_URL_BASE_API,
-    NEXT_PUBLIC_URI_SUPERTOKENS: process.env.NEXT_PUBLIC_URI_SUPERTOKENS,
-    NEXT_PUBLIC_API_KEY_SUPERTOKENS: process.env.NEXT_PUBLIC_API_KEY_SUPERTOKENS,
-    NEXT_PUBLIC_WEBSITE_DOMAIN: process.env.NEXT_PUBLIC_WEBSITE_DOMAIN,
-    NEXT_PUBLIC_ID_EXTENSION_CHROME: process.env.NEXT_PUBLIC_ID_EXTENSION_CHROME,
-    NEXT_PUBLIC_DSN_GLITCHTIP: process.env.NEXT_PUBLIC_DSN_GLITCHTIP
-  },
   async rewrites() {
     return [
       {
