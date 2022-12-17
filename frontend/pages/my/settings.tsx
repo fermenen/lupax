15cb35aeb3baadfa68ab8c2d1a5736a1e7a8fc23
Fernando M <fermenen2010@gmail.com>
2022-12-17 19:13:49 +0100

    refator settings

--- frontend/pages/my/settings.tsx
+++ frontend/pages/my/settings.tsx
@@ -6,6 +6,8 @@ import { getLayoutDashboard } from '../../layouts/layoutDashboard';
 import { useUser } from '../../services/users.service';
 import { ChangeEvent } from 'react';
 import { errorAlert } from '../../services/alert.service';
+import { DeleteIcon } from '@chakra-ui/icons';
+import { User } from '../../interfaces';
 
 import {
     VStack,
@@ -16,13 +18,16 @@ import {
     FormControl,
     FormLabel,
     Switch,
-    Skeleton
+    Skeleton,
+    Select,
+    Divider,
+    Button
 } from '@chakra-ui/react';
 
 import {
     FiUser,
     FiSettings,
-    FiBell
+    FiBell,
 } from 'react-icons/fi';
 
 
@@ -35,6 +40,75 @@ export default function Settings() {
 
     if (isError) return FailedToLoad("failed to load")
 
+    return (
+        <Main>
+            <BarPage title="Settings" />
+            <VStack
+                spacing={5}
+                align='stretch'>
+                {isLoading &&
+                    <>
+                        <Skeleton height='120px' />
+                        <Skeleton height='120px' />
+                        <Skeleton height='120px' />
+                    </>
+                }
+                {!isLoading &&
+                    <>
+                        <CardAccount user={user} />
+                        <Preferences />
+                        <Notifications user={user} mutate={mutate} />
+                    </>
+                }
+            </VStack>
+        </Main>
+    )
+
+}
+
+
+function CardAccount({ user }: { user: User }) {
+
+    return (
+        <CardIcon title={'Account'} icon={FiUser}>
+            <>
+                <Flex>
+                    <Avatar name={user.name} src={user.profile_picture} />
+                    <Box ml='3'>
+                        <Text fontWeight='bold'>
+                            {user.name} {user.last_name}
+                        </Text>
+                        <Text fontSize='sm'>{user.email}</Text>
+                    </Box>
+                </Flex>
+                <Divider mt={5} mb={5} />
+                <Button leftIcon={<DeleteIcon />} colorScheme='red' variant='solid' size='sm'>
+                    Delete account
+                </Button>
+            </>
+        </CardIcon>
+    )
+}
+
+function Preferences() {
+
+    return (
+        <CardIcon title={'Preferences'} icon={FiSettings}>
+            <FormControl>
+                <Box maxW='sm'>
+                    <FormLabel htmlFor='language'>Language</FormLabel>
+                    <Select>
+                        <option value='english-int'>English (int)</option>
+                    </Select>
+                </Box>
+
+            </FormControl>
+        </CardIcon>
+    )
+}
+
+
+function Notifications({ user, mutate }: { user: User, mutate: Function }) {
 
     const handleditPreferences = (preference: string, value: boolean) => {
 
@@ -52,69 +126,30 @@ export default function Settings() {
             if (!res.ok) throw Error(res.statusText);
             return res;
         }).then(function (data) {
-            mutate()
+            mutate();
         }).catch(error => {
             errorAlert("Modify alerts error", error.toString());
         })
     };
 
-
     return (
-        <>
-            <Main>
-                <BarPage title="Settings" />
-                <VStack
-                    spacing={5}
-                    align='stretch'>
-                    {isLoading &&
-                        <>
-                            <Skeleton height='120px' />
-                            <Skeleton height='120px' />
-                        </>
-                    }
-                    {!isLoading &&
-                        <CardIcon title={'Account'} icon={FiUser}>
-                            <>
-                                <Flex>
-                                    <Avatar name={user.name} src={user.profile_picture} />
-                                    <Box ml='3'>
-                                        <Text fontWeight='bold'>
-                                            {user.name} {user.last_name}
-                                        </Text>
-                                        <Text fontSize='sm'>{user.email}</Text>
-                                    </Box>
-                                </Flex>
-                            </>
-                        </CardIcon>
-                    }
-                    {/* <CardIcon title={'General'} icon={FiSettings}>
-                       
-                    </CardIcon> */}
-                    {!isLoading &&
-                        <CardIcon title={'Notifications'} icon={FiBell}>
-                            <VStack spacing={4} align={'stretch'}>
-                                <FormControl display='flex' alignItems='center'>
-                                    <FormLabel htmlFor='team_alerts' mb='0'>
-                                        Team alerts?
-                                    </FormLabel>
-                                    <Switch
-                                        id='team_alerts'
-                                        value={user.preferences.team_alerts}
-                                        isChecked={user.preferences.team_alerts}
-                                        colorScheme={'green'}
-                                        onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                                            var actual_selected = (event.currentTarget.value.toLowerCase() === 'true');
-                                            handleditPreferences('team_alerts', !actual_selected)
-                                        }}
-                                    />
-                                </FormControl>
-                            </VStack>
-                        </CardIcon>
-                    }
-                </VStack>
-            </Main>
-        </>
+        <CardIcon title={'Notifications'} icon={FiBell}>
+            <VStack spacing={4} align={'stretch'}>
+                <FormControl display='flex' alignItems='center'>
+                    <FormLabel htmlFor='team_alerts' mb='0'>
+                        Team alerts?
+                    </FormLabel>
+                    <Switch
+                        id='team_alerts'
+                        isChecked={user.preferences.team_alerts}
+                        colorScheme={'green'}
+                        onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                            var actual_selected = (event.currentTarget.value.toLowerCase() === 'true');
+                            handleditPreferences('team_alerts', !actual_selected)
+                        }}
+                    />
+                </FormControl>
+            </VStack>
+        </CardIcon>
     )
-
 }
-
